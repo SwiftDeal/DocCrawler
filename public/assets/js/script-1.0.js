32af07d6410e5fbcf54def4b9bc5e13b8347862b
Faizan Ayubi <indianayubi@gmail.com>
2015-11-12 21:25:55 +0530

    Modified Search form

--- public/assets/js/script-1.0.js
+++ public/assets/js/script-1.0.js
@@ -1,39 +1,33 @@
-/*
-$(document).ready(function() { 
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-
-    function initialize() {
-
-        var latlng = new google.maps.LatLng(52.3731, 4.8922);
-
-        var mapOptions = {
-            center: latlng,
-            scrollWheel: false,
-            zoom: 13
-        };
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            url: '/',
-            animation: google.maps.Animation.DROP
-        });
-
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        marker.setMap(map);
-
-    };
-});
-*/
-
 (function (window, $) {
     var $mapster = $('#map-canvas').mapster(Mapster.MAP_OPTIONS);
     
+    
+
     $mapster.mapster('addMarker', {
        lat: 37.791350,
        lng: -122.435883,
        content: 'I am here'
     });
     
+    $('#docSearch').submit(function() {
+        var self = $(this);
+
+        $.ajax({
+            url: 'doc/search',
+            type: 'GET',
+            data: self.serialize(),
+        })
+        .done(function(data) {
+            console.log(data);
+        })
+        .fail(function() {
+            console.log("error");
+            alert("Something went wrong, Please try again later")
+        })
+        .always(function() {
+            console.log("complete");
+        });
+        
+    });
 
 }(window, jQuery));
