5f2ceb84c758677c0b316fc75ab5b40294b893f8
Faizan Ayubi <indianayubi@gmail.com>
2015-11-14 03:23:12 +0530

    Search Done

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,210 +1,140 @@
-var $ = jQuery.noConflict();
+(function(window, $) {
 
+    $('.docSearch').submit(function(e) {
+        e.preventDefault();
+        $('#map-canvas').html('');
+        var $mapster = $('#map-canvas').mapster({
+	        center: {
+	            lat: 37.79135,
+	            lng: -122.435
+	        },
+	        zoom: 10,
+	        cluster: true,
+	        geocode: true
+	    });
+
+        var self = $(this);
+        self.addClass('main-blog');
+        $('#docSearch').html('<br><br><p><img src="http://searchforatopdoc.com/public/assets/images/loading.gif" alt="loader"></p>');
+        $.ajax({
+                url: 'doc/fetch.json',
+                type: 'GET',
+                data: self.serialize(),
+            })
+            .done(function(d) {
+                if (d.doctors[0]) {
+                    $mapster.mapster('reCenter', {
+                        lat: Number(d.doctors[0].location.lat),
+                        lng: Number(d.doctors[0].location.long)
+                    });
+                    $('#docSearch').html('<h4>Search Results for Top Doctors</h2>');
+                    $('#docSearch').append('<br><br>');
+                    $.each(d.doctors, function(i, val) {
+                        $mapster.mapster('addMarker', {
+                            lat: Number(val.location.lat),
+                            lng: Number(val.location.long),
+                            content: val.doctor.name + ', ' + val.doctor.suffix + '<br>' + val.doctor.practice + '<br>' + val.location.address + ', ' + val.location.city
+                        });
+                        $('#docSearch').append('<div class="blog-box"><div class="blog-text"><h4>' + val.doctor.name + '</h4><div class="post-meta">' + val.doctor.suffix + ', ' + val.doctor.practice + '<div class="clear"></div></div><div class="post-text">' + val.location.address + ', ' + val.location.city + '</div></div></div>');
+                    });
+                } else {
+                    $('#docSearch').html('<br><br><p>No Results Found, Please try some other location, <a href="/">go back</a></p>');
+                };
+            })
+            .fail(function() {
+                console.log("error");
+                alert("Something went wrong, Please try again later");
+            })
+            .always(function() {
+                console.log("complete");
+            });
+
+    });
+
+}(window, jQuery));
+
+var $ = jQuery.noConflict();
 $(document).ready(function($) {
 
-	/*-------------------------------------------------*/
-	/* =  Dropdown Menu - Superfish
-	/*-------------------------------------------------*/
-	try {
-		$('ul.sf-menu').superfish({
-			delay: 400,
-			autoArrows: false,
-			speed: 'fast',
-			animation: {opacity:'show', height:'show'}
-		});
-	} catch (err){
+    /*-------------------------------------------------*/
+    /* =  Dropdown Menu - Superfish
+    /*-------------------------------------------------*/
+    try {
+        $('ul.sf-menu').superfish({
+            delay: 400,
+            autoArrows: false,
+            speed: 'fast',
+            animation: {
+                opacity: 'show',
+                height: 'show'
+            }
+        });
+    } catch (err) {
 
-	}
+    }
 
-	/*-------------------------------------------------*/
-	/* =  Mobile Menu
-	/*-------------------------------------------------*/
-	// Create the dropdown base
+    /*-------------------------------------------------*/
+    /* =  Mobile Menu
+    /*-------------------------------------------------*/
+    // Create the dropdown base
     $("<select />").appendTo("#nav");
-    
+
     // Create default option "Go to..."
     $("<option />", {
-		"selected": "selected",
-		"value"   : "",
-		"text"    : "Go to..."
+        "selected": "selected",
+        "value": "",
+        "text": "Go to..."
     }).appendTo("#nav select");
-    
+
     // Populate dropdown with menu items
     $(".sf-menu a").each(function() {
-		var el = $(this);
-		$("<option />", {
-			"value"   : el.attr("href"),
-			"text"    : el.text()
-		}).appendTo("#nav select");
+        var el = $(this);
+        $("<option />", {
+            "value": el.attr("href"),
+            "text": el.text()
+        }).appendTo("#nav select");
     });
 
     $("#nav select").change(function() {
-		window.location = $(this).find("option:selected").val();
+        window.location = $(this).find("option:selected").val();
     });
-	
-	/*-------------------------------------------------*/
-	/* =  Scroll to TOP
-	/*-------------------------------------------------*/
-	$('a[href="#top"]').click(function(){
-        $('html, body').animate({scrollTop: 0}, 'slow');
-        return false;
-    });
-
-    	/*-------------------------------------------------*/
-	/* =  Input & Textarea Placeholder
-	/*-------------------------------------------------*/
-	$('input[type="text"], textarea').each(function(){
-		$(this).attr({'data-value': $(this).attr('placeholder')});
-		$(this).removeAttr('placeholder');
-		$(this).attr({'value': $(this).attr('data-value')});
-	});
-
-	$('input[type="text"], textarea').focus(function(){
-		$(this).removeClass('error');
-		if($(this).val().toLowerCase() === $(this).attr('data-value').toLowerCase()){
-			$(this).val('');
-		}	
-	}).blur( function(){ 
-		if($(this).val() === ''){
-			$(this).val($(this).attr('data-value'));
-		}
-	});
-
-
-       /*-------------------------------------------------*/
-	/* =  Tabs Widget - { Popular, Recent and Comments }
-	/*-------------------------------------------------*/
-	$('.tab-links li a').live('click', function(e){
-		e.preventDefault();
-		if (!$(this).parent('li').hasClass('active')){
-			var link = $(this).attr('href');
-
-			$(this).parents('ul').children('li').removeClass('active');
-			$(this).parent().addClass('active');
-
-			$('.tabs-widget > div').hide();
-
-			$(link).fadeIn();
-		}
-	});
 
     /*-------------------------------------------------*/
-	/* =  Testimonials
-	/*-------------------------------------------------*/
+    /* =  Input & Textarea Placeholder
+    /*-------------------------------------------------*/
+    $('input[type="text"], textarea').each(function() {
+        $(this).attr({
+            'data-value': $(this).attr('placeholder')
+        });
+        $(this).removeAttr('placeholder');
+        $(this).attr({
+            'value': $(this).attr('data-value')
+        });
+    });
 
-	try { //fade effect
-		$('.bxslider.fade').bxSlider({
-			adaptiveHeight: true,
-			mode: 'fade',
-			pager: false
-		});
-	} catch(err) {
+    $('input[type="text"], textarea').focus(function() {
+        $(this).removeClass('error');
+        if ($(this).val().toLowerCase() === $(this).attr('data-value').toLowerCase()) {
+            $(this).val('');
+        }
+    }).blur(function() {
+        if ($(this).val() === '') {
+            $(this).val($(this).attr('data-value'));
+        }
+    });
 
-	}
+    /*-------------------------------------------------*/
+    /* =  Testimonials
+    /*-------------------------------------------------*/
 
-	/* ---------------------------------------------------------------------- */
-	/*	Contact Map
-	/* ---------------------------------------------------------------------- */
-	var contact = {"lat":"40.714623", "lon":"-74.006605"}; //Change a map coordinate here!
+    try { //fade effect
+        $('.bxslider.fade').bxSlider({
+            adaptiveHeight: true,
+            mode: 'fade',
+            pager: false
+        });
+    } catch (err) {
 
-	try {
-		$('#map').gmap3({
-		    action: 'addMarker',
-		    latLng: [contact.lat, contact.lon],
-		    map:{
-		    	center: [contact.lat, contact.lon],
-		    	zoom: 14
-		   		},
-		    },
-		    {action: 'setOptions', args:[{scrollwheel:false}]}
-		);
-	} catch(err) {
+    }
 
-	}
-
-	/* ---------------------------------------------------------------------- */
-	/*	Contact Form
-	/* ---------------------------------------------------------------------- */
-
-	var submitContact = $('#submit_contact'),
-		message = $('#msg');
-
-	submitContact.on('click', function(e){
-		e.preventDefault();
-
-		var $this = $(this);
-		
-		$.ajax({
-			type: "POST",
-			url: 'contact.php',
-			dataType: 'json',
-			cache: false,
-			data: $('#contact-form').serialize(),
-			success: function(data) {
-
-				if(data.info !== 'error'){
-					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
-					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				} else {
-					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				}
-			}
-		});
-	});
-
-	/* ---------------------------------------------------------------------- */
-	/*	Appointment Form
-	/* ---------------------------------------------------------------------- */
-
-	var submitContact = $('#submit_contact2'),
-		message = $('#msg2');
-
-	submitContact.on('click', function(e){
-		e.preventDefault();
-
-		var $this = $(this);
-		
-		$.ajax({
-			type: "POST",
-			url: 'appointment.php',
-			dataType: 'json',
-			cache: false,
-			data: $('#contact-form2').serialize(),
-			success: function(data) {
-
-				if(data.info !== 'error'){
-					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
-					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				} else {
-					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-				}
-			}
-		});
-	});
-
-	/* ---------------------------------------------------------------------- */
-	/*	Portfolio
-	/* ---------------------------------------------------------------------- */
-	
-	$('#filterOptions li a').click(function() {
-		// fetch the class of the clicked item
-		var ourClass = $(this).attr('class');
-		
-		// reset the active class on all the buttons
-		$('#filterOptions li').removeClass('active');
-		// update the active state on our clicked button
-		$(this).parent().addClass('active');
-		
-		if(ourClass == 'all') {
-			// show all our items
-			$('#ourHolder').children('div.item').show();	
-		}
-		else {
-			// hide all elements that don't share ourClass
-			$('#ourHolder').children('div:not(.' + ourClass + ')').hide();
-			// show all elements that do share ourClass
-			$('#ourHolder').children('div.' + ourClass).show();
-		}
-		return false;
-	});
 });
